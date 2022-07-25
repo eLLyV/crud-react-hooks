885c05eb123d11d4981006de79790ab2e427c7d2
elizastudiomade <67680907+elizastudiomade@users.noreply.github.com>
2022-07-25 15:49:24 +0100

    read users

---
+++ src/tables/UserTable.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const UserTable = (props) => (
+    <table>
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Username</td>
+                <td>Actions</td>
+            </tr>
+        </thead>
+        <tbody>
+            {props.users.length > 0 ? (
+                props.users.map((user) => (
+                    <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.username}</td>
+                        <td>
+                            <button className='button muted-button'>Edit</button>
+                            <button className='button muted-button'>Delete</button>
+                        </td>
+                    </tr>
+                ))
+            ) : (
+                <tr>
+                    <td colSpan={3}>No users</td>
+                </tr>
+            )}
+        </tbody>
+    </table>
+)
+
+export default UserTable
